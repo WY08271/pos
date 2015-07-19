c23aeb932246e82e8d1a85b39c33691595933308
WY08271 <wywy08271@outlook.com>
2015-07-19 11:57:55 +0800

    v2-v2.3
    faultiness

--- pos_v2_3rdlib/main/main.js
+++ pos_v2_3rdlib/main/main.js
@@ -1,1 +1,101 @@
-//TODO: Please write code in this file.
+dateDigitToString = function(num) {
+      return num < 10 ? '0' + num : num;
+  }
+function printReceipt(inputs){
+	var result = '***<没钱赚商店>收据***\n';
+	var allItems = loadAllItems(); 
+	var total = 0;
+	var promotions = loadPromotions();
+	var barcodes = promotions[0].barcodes;
+	var promot = 0;
+    var currentDate = new Date(),
+      year = dateDigitToString(currentDate.getFullYear()),
+      month = dateDigitToString(currentDate.getMonth() + 1),
+      date = dateDigitToString(currentDate.getDate()),
+      hour = dateDigitToString(currentDate.getHours()),
+      minute = dateDigitToString(currentDate.getMinutes()),
+      second = dateDigitToString(currentDate.getSeconds()),
+    formattedDateString = year + '年' + month + '月' + date + '日 ' + hour + ':' + minute + ':' + second;
+    result += '打印时间：' + formattedDateString + '\n' + '----------------------\n';
+
+	for(var i = 0; i < inputs.length; i++)
+	{
+		if(inputs[i].length < 11)
+		{
+			var count = 1;
+			for(var k = 1 + i ; k < inputs.length ; k ++)
+			{
+				if( inputs[i] == inputs[k] )
+				{
+					count = count + 1 ; 
+					inputs.splice( k , 1 );
+					k --;
+                }
+            }
+            for(var x = 0; x < allItems.length; x++)
+            {
+               	if(inputs[i] == allItems[x].barcode)
+				{
+					var temp = barcodes.indexOf(inputs[i]);
+                    if(temp !== -1)
+
+                    {
+
+					  result = result + '名称：' + allItems[x].name + '，数量：' + count + allItems[x].unit + '，单价：' 
+    		                 + allItems[x].price.toFixed(2) +'(元)，小计：'
+ 	                         + ((count - 1) * allItems[x].price).toFixed(2) + '(元)\n' ;
+ 	                  total += count * allItems[x].price; 
+ 	                  break;                   	
+                    }
+					  result = result + '名称：' + allItems[x].name + '，数量：' + count + allItems[x].unit + '，单价：' 
+    		                 + allItems[x].price.toFixed(2) +'(元)，小计：'
+ 	                         + (count * allItems[x].price).toFixed(2) + '(元)\n' ;
+ 	                  total += count * allItems[x].price;
+    	        }  
+            }
+		}
+		else
+		{
+			var string = [];
+			string = inputs[i].split('-');
+			for(var x = 0; x < allItems.length; x++)
+			{
+				if(string[0] == allItems[x].barcode)
+				{
+					var temp = barcodes.indexOf(inputs[i]);
+					if(temp == 1)
+					{
+						string[1] -= 1;
+					}
+					result = result + '名称：' + allItems[x].name + '，数量：' + string[1] + allItems[x].unit + '，单价：' 
+    		                + allItems[x].price.toFixed(2) +'(元)，小计：'
+ 	                        + (string[1] * allItems[x].price).toFixed(2) + '(元)\n' ;
+ 	                total += string[1] * allItems[x].price;
+				}
+    	    }
+        }
+	}
+		var save = 0;
+		result +=  '----------------------\n' + '挥泪赠送商品：\n' ;
+		for(var i = 0; i < barcodes.length; i++)
+		{
+			for(var k = 0; k < inputs.length; k++)
+			{
+				if(barcodes[i] == inputs[k])
+				{
+					for(var x = 0; x < allItems.length; x++)
+					{
+						if(inputs[k] == allItems[x].barcode)
+						{
+					        result += '名称：' + allItems[x].name + '，数量：1' + allItems[x].unit + '\n';
+					        save += 1 * allItems[x].price.toFixed(2);							
+						}
+					}
+				}
+			}
+		}
+		result += '----------------------\n' + '总计：' + (total-save).toFixed(2) + '(元)\n' + '节省：' + save.toFixed(2) +
+		       '(元)\n' + '**********************';
+	console.log(result);
+}
+
