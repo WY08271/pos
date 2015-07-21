3c297fa4be788529fc39bcc21ec4b3174d7d2fa4
wy <wy08271@outlook.com>
2015-07-21 23:11:19 +0800

    7

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -7,7 +7,6 @@ function printReceipt(barcodes) {
   var promotionsBarcodes = getPromotionsBarcodes(promotions);
   var discountCartItems = getDiscountCartItems(promotionsBarcodes , items);
 
-
   var receipt =
     '***<没钱赚商店>收据***\n' +
     getItemsString(cartItems , discountCartItems ) +
@@ -149,7 +148,6 @@ function getAmount(cartItems) {
   });
   return amount;
 }
-
 
 function getItemsString(cartItems , discountCartItems) {
   var itemsString = '';
