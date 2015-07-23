cc3aeacd4dbe88a8573c13eb9d84fb9c208b134a
wy <wy08271@outlook.com>
2015-07-23 10:55:14 +0800

    V2 Math.floor

--- pos_v2/main/main.js
+++ pos_v2/main/main.js
@@ -2,7 +2,7 @@ function printReceipt(tags) {
   var cartItems = [];
    cartItems = new CartItem().cartItemList(cartItems, tags);
   var  discount = new discountCartItem();
-  var a = discount.promotionType(cartItems);
-  var receipt = new PrintCartItems().receipt(cartItems , a);
+  discount.promotionType(cartItems);
+  var receipt = new PrintCartItems().receipt(cartItems);
   console.log(receipt);
 }
